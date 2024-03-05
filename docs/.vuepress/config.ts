3cf6cf3dafcc9d2af052fc8a9fae940a647507c2
GCSLaoLi <cnlidong@live.cn>
2024-03-05 11:42:23 +0800

    增加违规公示

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -12,6 +12,7 @@ export default defineUserConfig({
     navbar: [
         { text: '首页', link: '/' },
         { text: '反代教程', link: '/guide/' },
+        { text: '违规公示', link: '/shame/' },
         { text: '公共镜像', link: 'https://chat.freegpts.org' },
         { text: '免费车队', link: 'https://share.freegpts.org' },
         { text: '交流社区', link: 'https://github.com/orgs/freegpts-org/discussions' },
